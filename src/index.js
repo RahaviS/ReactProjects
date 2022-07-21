761c11d4a2813043ee00250647972c72c8ff735a
RahaviS <108854458+RahaviS@users.noreply.github.com>
2022-07-21 10:57:02 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>I am Rahavi</p>
+  </div>,
+  document.getElementById("root")
+);
