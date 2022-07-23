7433cc05a3b7c52ebd5b372d4fd216c394476283
RahaviS <108854458+RahaviS@users.noreply.github.com>
2022-07-23 11:24:32 +0530

    added random number

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ ReactDOM.render(
   <div>
     <h1> Hello {name} </h1>
     <p>My Lucky number is {luckyNo} </p>
+    <p>My Random Number is {Math.floor(Math.random() * 10)} </p>
   </div>,
   document.getElementById("root")
 );
